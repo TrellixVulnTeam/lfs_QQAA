eee430e7a9bf46e5fd03705b0353d56999e93cc9
Nobody <rich.freeman.paul@gmail.com>
2020-02-06 09:53:22 -0500

    Check with mountpoint before mounting filesystems

--- bin/mounts.sh
+++ bin/mounts.sh
@@ -1,5 +1,6 @@
-
- mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
- mount -vt proc proc $LFS/proc
- mount -vt sysfs sysfs $LFS/sys
- mount -vt tmpfs tmpfs $LFS/run
+LFS=/lfs
+mountpoint $LFS/dev || mount -vt devfs $LFS/dev
+mountpoint $LFS/dev/pts || mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+mountpoint $LFS/proc || mount -vt proc proc $LFS/proc
+mountpoint $LFS/sys || mount -vt sysfs sysfs $LFS/sys
+mountpoint $LFS/run || mount -vt tmpfs tmpfs $LFS/run
