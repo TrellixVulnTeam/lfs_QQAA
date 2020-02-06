1cbaf6d6f7ebb3e1b02b8c61b2a51fb5f5cd87d5
Nobody <rich.freeman.paul@gmail.com>
2020-02-06 09:56:00 -0500

    removed bin/env.ch7.sh

--- bin/env.ch7.sh
+++
@@ -1,7 +0,0 @@
-LFS=/lfs
-LFS_TGT=$(uname -m)-lfs-linux-gnu
-LFS_SRC=${LFS}/usr/src/lfs
-LFS_ARC=${LFS_SRC}/arc
-LFS_BLD=${LFS_SRC}/src/ch7
-PATH=/tools/bin:/bin:/usr/bin
-export LFS LFS_TGT LFS_SRC LFS_ARC LFS_BLD
