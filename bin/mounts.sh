e304be361ae59be8b37a7a28dacc0de3f9cfe87b
Nobody <rich.freeman.paul@gmail.com>
2020-02-06 09:54:21 -0500

    Check mountpoints before mounting

--- bin/mounts.sh
+++ bin/mounts.sh
@@ -1,5 +1,14 @@
+LFS=/lfs
 
- mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
- mount -vt proc proc $LFS/proc
- mount -vt sysfs sysfs $LFS/sys
- mount -vt tmpfs tmpfs $LFS/run
+set -e
+for i in dev dev/pts proc sys; do
+  mkdir -p $LFS/$i
+done
+mountpoint $LFS/dev ||
+  mount --bind /dev $LFS/dev
+mountpoint $LFS/dev/pts || 
+  mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+mountpoint $LFS/proc/ ||
+  mount -vt proc proc $LFS/proc
+mountpoint $LFS/sys/ ||
+  mount -vt sysfs sysfs $LFS/sys
