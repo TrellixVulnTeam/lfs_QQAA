8961ead324f567f7f55d17b5eb4359cc593ba05c
Nobody <rich.freeman.paul@gmail.com>
2020-06-05 09:52:15 -0400

    x

--- bin/env.ch5.sh
+++
@@ -1,9 +0,0 @@
-PART=ch5
-LFS=/lfs
-LFS_TGT=$(uname -m)-lfs-linux-gnu
-LFS_SRC=/lfs/usr/src/lfs
-LFS_ARC=${LFS_SRC}/arc/lfs
-LFS_BLD=${LFS_SRC}/src/$PART
-LFS_DST=/static
-PATH=/tools/bin:/bin:/usr/bin
-export LFS LFS_TGT LFS_SRC LFS_ARC LFS_BLD PART
