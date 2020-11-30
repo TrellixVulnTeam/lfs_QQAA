489e227a8c6ca27a4fb02345b49b0c677e7439b4
nobody <github2@invisiblehand.church>
2020-11-30 10:39:11 -0500

    cleaned up generation

--- bin/env.ch5.sh
+++ bin/env.ch5.sh
@@ -1,9 +1,18 @@
+set -e
 PART=ch5
-LFS=/lfs
-LFS_TGT=$(uname -m)-lfs-linux-gnu
-LFS_SRC=/lfs/usr/src/lfs
-LFS_ARC=${LFS_SRC}/arc/lfs
-LFS_BLD=${LFS_SRC}/src/$PART
+LFS=/boot
 LFS_DST=/static
-PATH=/tools/bin:/bin:/usr/bin
+LFS_TGT=$(uname -m)-lfs-linux-gnu
+LFS_SRC=${LFS_DST}/src/lfs
+cd "${LFS_SRC}"
+LFS_ARC=${LFS_SRC}/arc/lfs
+LFS_BLD=${LFS_SRC}/src
+set -- ${LFS_DST}/ / /usr --
+while [[ $1 != -- ]]; do
+  i="$1";
+  set "$@" ${1%/}/sbin ${1%/}/bin
+  shift
+done
+shift
+eval "$( IFS=:; echo PATH="$*" )"
 export LFS LFS_TGT LFS_SRC LFS_ARC LFS_BLD PART
